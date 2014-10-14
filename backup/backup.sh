f0e14b59eaa561f5cab46b85aa849bda8d66e65a
Mateus Caruccio <mateus@caruccio.com>
2014-10-14 18:05:34 -0300

    Refactored + rotation

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,98 +1,106 @@
 #!/bin/bash
- 
 
+#
+# Configs
+#
+
+# Where to store backups
 BACKUP_DIR="${OPENSHIFT_DATA_DIR}backup/"
+
+# Number of days to keep backup files
+CYCLE_DAYS=7
+
+# Space-separated list of mongodb databases to backup
+MONGODB_DATABASES="$OPENSHIFT_APP_NAME"
+
+# Don't need to touch below
+#####################################################
+
+set -eu
 
 if [ ! -d $BACKUP_DIR ]; then
 	mkdir -p $BACKUP_DIR
 fi
 
-DATE=`date +%d-%m-%Y_%H_%M`
+SECONDS_IN_A_DAY=86400
+TIMESTAMP=`date +%s`
+ISODATE=`date --rfc-3339=seconds --date=@$TIMESTAMP | tr -s ' ' _` # Format: 1970-01-01_00:00:00+00:00
+function from_iso() { date --date="${1/_/ }" +%s; }
 
+CRON_PERIOD=${0##*/cron/}
+CRON_PERIOD=${CRON_PERIOD%%/*}
+case "$CRON_PERIOD" in
+	minutely|hourly|daily) SENTINEL=$[ TIMESTAMP - SECONDS_IN_A_DAY ];;
+	*) SENTINEL=${SENTINEL:-0}
+esac
 
+function info() { echo "[`date`]" INFO $*; }
+function error() { echo "[`date`]" ERROR $*; }
 
-mysql() {
-	
-	DB_USER=$OPENSHIFT_MYSQL_DB_USERNAME
-	DB_PASS=$OPENSHIFT_MYSQL_DB_PASSWORD
-	DB_HOST=$OPENSHIFT_MYSQL_DB_HOST
-	DB_PORT=$OPENSHIFT_MYSQL_DB_PORT
-	FINAL_BACKUP_DIR=$BACKUP_DIR"mysql/"
-	DB_PARAM='--add-drop-table --add-locks --extended-insert --single-transaction -quick'
-	BACKUP_NAME="${DATE}"
+function remove_old_backup()
+{
+	local DIR=$1
+	cd $DIR
+	for FILE in *.gz; do
+		if [ -f "${FILE}" ]; then
+			FILE_DATE="${FILE%%.*}"
+			FILE_DATE="${FILE_DATE##*/}"
+			FILE_TIMESTAMP=`from_iso $FILE_DATE`
+			if [ "$FILE_TIMESTAMP" -lt $SENTINEL ]; then
+				rm -fv "./$FILE"
+			fi
+		fi
+	done
+}
 
-	if [ ! -d  $FINAL_BACKUP_DIR ]; then
-		mkdir $FINAL_BACKUP_DIR
+function backup_database()
+{
+	local NAME="$1"
+	shift
+	local CMD="$*"
+	if [ ! -d $BACKUP_DIR/$NAME ]; then
+		mkdir $BACKUP_DIR/$NAME
 	fi
+	info Start backup: $NAME
 
-	echo -e "\nBacking up MySQL databases..."
-	if ! mysqldump $DB_PARAM  -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS --all-databases | gzip > "${FINAL_BACKUP_DIR}${BACKUP_NAME}.sql.gz"; then
-		echo "[!!ERROR!!] Failed to produce mysql backup database $DB_NAME"
+	cd $BACKUP_DIR/$NAME
+	info "-> " "[${BACKUP_DIR}${NAME}/${ISODATE}.gz]"
+	if $CMD | gzip > "${BACKUP_DIR}${NAME}/${ISODATE}.gz"; then
+		remove_old_backup $PWD
+		info Finished backup: $NAME
+	else
+		error Backup failed: $NAME
 	fi	 
 
-	echo -e "\nMySQL Backup Complete!"
 }
 
-postgresql() {
-
-
-	DB_HOST=$OPENSHIFT_POSTGRESQL_DB_HOST
-	DB_PORT=$OPENSHIFT_POSTGRESQL_DB_PORT
-	DB_USER=$OPENSHIFT_POSTGRESQL_DB_USERNAME
-	DB_PASS=$OPENSHIFT_POSTGRESQL_DB_PASSWORD
-	DB_NAME=$OPENSHIFT_APP_NAME
-	FINAL_BACKUP_DIR=$BACKUP_DIR"postgresql/"
-	BACKUP_NAME="${DATE}"
- 
- 	if [ ! -d $FINAL_BACKUP_DIR ]; then
-		mkdir $FINAL_BACKUP_DIR
-	fi
- 
-	echo -e "\nBacking up PostgreSQL databases..." 
-	if ! pg_dumpall -c -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" | gzip > "${FINAL_BACKUP_DIR}${BACKUP_NAME}".sql.gz.in_progress; then
-		echo "[!!ERROR!!] Failed to produce postgresql backup database $DB_NAME"
-	else
-		mv "${FINAL_BACKUP_DIR}${BACKUP_NAME}".sql.gz.in_progress "${FINAL_BACKUP_DIR}${BACKUP_NAME}".sql.gz
-	fi
-	 
-	echo -e "\nPostgreSQL backup complete!"
+function mysql()
+{
+	
+	local DB_PARAM='--add-drop-table --add-locks --extended-insert --single-transaction -quick'
+	backup_database mysql mysqldump $DB_PARAM -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME -p$OPENSHIFT_MYSQL_DB_PASSWORD --all-databases
 }
 
-
-mongodb() {
-
-	DB_HOST=$OPENSHIFT_MONGODB_DB_HOST
-	DB_PORT=$OPENSHIFT_MONGODB_DB_PORT
-	DB_USER=$OPENSHIFT_MONGODB_DB_USERNAME
-	DB_PASS=$OPENSHIFT_MONGODB_DB_PASSWORD
-	DB_NAME=$OPENSHIFT_APP_NAME
-	FINAL_BACKUP_DIR=$BACKUP_DIR"mongodb/"
-	BACKUP_NAME="${DATE}"
-
-	if [ ! -d $FINAL_BACKUP_DIR ]; then
-		mkdir $FINAL_BACKUP_DIR
-	fi
-
-	echo -e "\nBacking up MongoDB databases..."
-	if ! mongodump --host $DB_HOST --port $DB_PORT --username $DB_USER --password $DB_PASS --out "${FINAL_BACKUP_DIR}dump/" > /dev/null; then
-		echo "[!!ERROR!!] Failed to produce mongo backup database $DATABASE"
-	else
-		cd $FINAL_BACKUP_DIR
-		tar cfz "${BACKUP_NAME}.tar.gz" dump
-		rm -rf dump
-	fi
-
-	echo -e "\nMongodb Backup Complete!"
+function postgresql()
+{
+	backup_database postgresql pg_dumpall -c -h "$OPENSHIFT_POSTGRESQL_DB_HOST" -p "$OPENSHIFT_POSTGRESQL_DB_PORT" -U "$OPENSHIFT_POSTGRESQL_DB_USERNAME"
 }
 
-if [ ! -z $OPENSHIFT_MYSQL_DB_HOST ]; then
-	mysql
-fi
+function mongodb_dump()
+{
+	local DBNAME=$1
+	local DUMPDIR=dump_$TIMESTAMP
+	mongodump --db $DBNAME --host $OPENSHIFT_MONGODB_DB_HOST --port $OPENSHIFT_MONGODB_DB_PORT --username $OPENSHIFT_MONGODB_DB_USERNAME --password $OPENSHIFT_MONGODB_DB_PASSWORD --out $DUMPDIR > /dev/null
+	tar cf - $DUMPDIR && rm -rf $DUMPDIR >/dev/null || rm -rf $DUMPDIR >/dev/null
+}
 
-if [ ! -z $OPENSHIFT_POSTGRESQL_DB_HOST ]; then
-	postgresql
-fi
+function mongodb()
+{
+	for _DB in $MONGODB_DATABASES; do
+		backup_database mongodb mongodb_dump $_DB
+	done
+}
 
-if [ ! -z $OPENSHIFT_MONGODB_DB_HOST ]; then
-	mongodb
-fi
+[ ${OPENSHIFT_MYSQL_DB_HOST}      ] && mysql
+[ ${OPENSHIFT_POSTGRESQL_DB_HOST} ] && postgresql
+[ ${OPENSHIFT_MONGODB_DB_HOST}    ] && mongodb
